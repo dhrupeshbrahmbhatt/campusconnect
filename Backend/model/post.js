cad8013bad80cf9d85d8105f6a5cc04e091637fd
bhavye-thakkar <thakkarbhavye1425@gmail.com>
2024-09-11 13:20:28 +0530

    Post schema fix

--- Backend/model/post.js
+++ Backend/model/post.js
@@ -1,20 +1,75 @@
-const mongoose = require("mongoose");
-const Post_Schema = new mongoose.Schema(
-    {
-        id: {
-            type: mongoose.Types.ObjectId,
-            require:true,
-        },
-        title: {
-            type: String,
-            require: true
-        },
-        content: {
-            type: String,
-            require: true,
-        },
-    }
-)
+const mongoose = require('mongoose');
+const User = require("./user");
 
-const Posts = mongoose.model('CamConnect_posts', Post_Schema);
-module.exports = Posts
+// comment schema
+const CommentSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: User,
+    },
+    content: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        require: true
+    },
+    likes: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:  User,
+        require: true
+    }
+})
+// Post Schema 
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+        require: true
+    },
+    content: {
+        type:String,
+        required: true
+    },
+    media: {
+        type: String,
+        require: false
+    },
+    likes: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    },
+    comments: [CommentSchema],
+    createdAt: {
+        type:Date,
+        require: true
+    },
+    updatedAt: {
+        type: Date,
+        require: true
+    },
+    isEdited: {
+        type: Boolean,
+        default: false
+    },
+    reposts: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post"
+    },
+    sharedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+        require: false
+    },
+    hashtags: [{
+        type:String,
+    }],
+    mentions: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    }]
+});
+
+module.exports = mongoose.model("Post", PostSchema);
